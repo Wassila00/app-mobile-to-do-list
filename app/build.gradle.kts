df8ed8dad0b986708e3f4b1e37bd6045e21815ca
Masaaf Badr Eddine <badrsatisfiying@gmail.com>
2025-05-18 21:40:32 +0100

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(libs.recyclerview)
+    implementation("org.tensorflow:tensorflow-lite:2.12.0")
 
 
 }
